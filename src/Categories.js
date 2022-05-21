a3e3d45647bad2e087afd37b71c0421aeed5c1b3
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-21 21:25:34 +0300

    implementing dynamic categories

--- src/Categories.js
+++ src/Categories.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Categories = ({ filterItems }) => {
   return (
     <div className="btn-container">
+      <button className="filter-btn" onClick={() => filterItems('усе меню')}>
+        усе меню
+      </button>
       <button className="filter-btn" onClick={() => filterItems('обід')}>
         обід
       </button>
